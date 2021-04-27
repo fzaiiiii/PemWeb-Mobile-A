defaadc1f462c7eb330a7f84496a644901da4236
fzaiiiii <81138144+fzaiiiii@users.noreply.github.com>
2021-04-28 03:20:27 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,36 @@
-<?php
-require("koneksi.php");
-$data = mysqli_query($kon, "SELECT * FROM pegawai");
-?>
 
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Modul 3</title>
-</head>
+<head></head>
 <body>
-    <h3>Daftar Data Pegawai</h3>
-    <a href="penyimpanan.php">Tambah Data</a>
+<table>
+<thead>
+  <tr>
+      <th>No</th>
+      <th>ID</th>
+      <th>Nama</th>
+      <th>Jenis Kelamin</th>
+      <th>Jenis</th>
+  </tr>
+</thead>
+<tbody>
 
-    <table border="1px">
-    <tr>
-        <th>ID Pegawai</th>
-        <th>Nama</th>
-        <th>Tempat Tanggal Lahir</th>
-        <th>Alamat</th>
-        <th>Gaji</th>
-    </tr>
+<?php
+  require 'koneksi.php';
+  $no = 0;
+  $view = $koneksi -> query("SELECT * FROM `t_pengunjung` ");
+  while($row=$view->fetch_array()){
+  $no++;
+?> 
+  <tr>
+      <td><?php echo $no ?></td>
+      <td><?php echo $row['id']; ?></td>  
+      <td><?php echo $row['nama']; ?></td>  
+      <td><?php echo $row['jk']; ?></td>              
+      <td><?php echo $row['jenis']; ?></td>                                                     
+  </tr>						
 
-    <?php while($row = mysqli_fetch_assoc($data)){ ?>
-    <tr>
-        <td><?php echo $row['id'] ?></td>
-        <td><?php echo $row['nama'] ?></td>
-        <td><?php echo $row['ttl'] ?></td>
-        <td><?php echo $row['alamat'] ?></td>
-        <td><?php echo $row['gaji'] ?></td>
-        <td>
-            <a href="update.php?id=<?php echo $row['id']; ?>">Edit</a> | 
-            <a href="delete.php?id=<?php echo $row['id']; ?>">Hapus</a>
-        </td>
-    </tr>
-    <?php
-    } // end while
-    
-    mysqli_close($kon); // menutup koneksi ke database
-    ?>
-    </table>
-</body>
+<?php }?>
+  </tbody>
+  </table>
+  </body>
 </html>
--- koneksi.php
+++ koneksi.php
@@ -1,21 +1,15 @@
-<?php
+<?php 
 
-    $host="localhost";
-    $user="root";
-    $password="";
-    $db="mod3";
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'perpustakaan';
 
-    $kon = mysqli_connect($host,$user,$password);
-    //if ($kon){
-	//    echo "Database MYSQL <b>berhasil</b> dikoneksikan<br>";
-    //}else {
-	//    echo"Database  MYSQL <b>gagal</b> dikoneksikan<br>";
-    //}
+$koneksi = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 
-    $hasil=mysqli_select_db($kon,$db);
-    //if ($hasil){
-	//    echo "Database $db berhasil dipilih";
-    //}else {
-	//echo "Database $db gagal dipilih";
-    //}
-?>
+if ($koneksi->connect_error) 
+{
+	die('Database Tidak Terhubung :'. $koneksi->connect_error);
+} 
+
+ ?>
